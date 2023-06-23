8aebeb43e3cc993eafbc473939115ca6d9d3ea7d
clebertmarctyson <marctysonclebert9@gmail.com>
2023-06-23 18:37:10 -0400

    Initialize Project

--- dist/jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    testMatch: ["**/test/**/*.test.ts"],
-    roots: ["./test"],
-    moduleFileExtensions: ["ts", "js"],
-    modulePathIgnorePatterns: ["./node_modules"],
-};
