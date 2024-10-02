35578b932f06bbb6943d08550644cf5b5d4ef612
clebertmarctyson <marctysonclebert9@gmail.com>
2024-10-02 10:47:22 -0400

    Fix: Update import statements to use relative paths

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
-// src/index.ts
-import * as validation from "@/validation";
-import * as conversion from "@/conversion";
-import * as subnet from "@/subnet";
-import * as operations from "@/operations";
+import * as validation from "./validation";
+import * as conversion from "./conversion";
+import * as subnet from "./subnet";
+import * as operations from "./operations";
 
 export { validation, conversion, subnet, operations };
 
 // Also export all functions individually
-export * from "@/validation";
-export * from "@/conversion";
-export * from "@/subnet";
-export * from "@/operations";
+export * from "./validation";
+export * from "./conversion";
+export * from "./subnet";
+export * from "./operations";
 
 // Create a default export object
 const ipNavigator = {
